089d18246e5e896ff69bd06d6c1d93b994c55526
raknor90 <bkohnert@gmail.com>
2024-12-04 12:35:48 +0100

    Linked Buildfunctions

--- ct/jdownloader.sh
+++ ct/jdownloader.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(cat ./build.func)
+source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2021-2024 tteck
 # Author: tteck (tteckster)
 # License: MIT

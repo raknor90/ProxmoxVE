0c40188937b50da0040b20f792d82edc3fd4802c
raknor90 <bkohnert@gmail.com>
2024-12-04 20:03:16 +0100

    .

--- ct/jdownloader.sh
+++ ct/jdownloader.sh
@@ -25,7 +25,7 @@ var_ram="512"
 var_os="alpine"
 var_version="3.19"
 
-echo 'HIER: '$CT_ID', '$NEXTID
+
 variables
 color
 catch_errors
@@ -67,31 +67,32 @@ msg_ok "Updated ${APP} LXC"
 exit
 }
 
-:'
+
 start
-build_container
-description
+echo 'HIER: '$CT_ID', '$NEXTID
+
+# build_container
+# description
 
 
-msg_info "Mount download dir from cifs share"
+# msg_info "Mount download dir from cifs share"
 
-read -p "Enter the folder name (e.g., nas_rwx): " folder_name
-read -p "Enter the CIFS hostname or IP (e.g., NAS): " cifs_host
-read -p "Enter the share name (e.g., media): " share_name
-read -p "Enter SMB username: " smb_username
-read -sp "Enter SMB password: " smb_password && echo
-read -p "Enter the LXC ID: " lxc_id
-read -p "Enter the username within the LXC that needs access to the share (e.g., jellyfin, plex): " lxc_username
+# read -p "Enter the folder name (e.g., nas_rwx): " folder_name
+# read -p "Enter the CIFS hostname or IP (e.g., NAS): " cifs_host
+# read -p "Enter the share name (e.g., media): " share_name
+# read -p "Enter SMB username: " smb_username
+# read -sp "Enter SMB password: " smb_password && echo
+# read -p "Enter the LXC ID: " lxc_id
+# read -p "Enter the username within the LXC that needs access to the share (e.g., jellyfin, plex): " lxc_username
 
-bash -c "$(wget -qO - https://gist.githubusercontent.com/NorkzYT/14449b247dae9ac81ba4664564669299/raw/7d2d0fce37a8896823c9035a2e765d14a96058c0/proxmox-lxc-cifs-share.sh)" << ANSWER
-$folder_name
-$cifs_host
-$share_name
-$smb_username
-$smb_password
-$lxc_id
-$lxc_username
-ANSWER
-msg_ok "Mount download dir from cifs share"
-msg_ok "Completed Successfully!\n"
-'
+# bash -c "$(wget -qO - https://gist.githubusercontent.com/NorkzYT/14449b247dae9ac81ba4664564669299/raw/7d2d0fce37a8896823c9035a2e765d14a96058c0/proxmox-lxc-cifs-share.sh)" << ANSWER
+# $folder_name
+# $cifs_host
+# $share_name
+# $smb_username
+# $smb_password
+# $lxc_id
+# $lxc_username
+# ANSWER
+# msg_ok "Mount download dir from cifs share"
+# msg_ok "Completed Successfully!\n"
